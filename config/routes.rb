234077b801b6eab95fc2ca05fc5d9e2884bc406f
javiera <javieraisoto.03@gmail.com>
2023-06-05 01:06:49 -0400

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'contact/index'
+  get 'projects/index'
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/projects', to: 'projects#index'
+  get '/contact', to: 'contact#index'
+  # Defines the root path route ("/")
+  root "home#index"
+end
